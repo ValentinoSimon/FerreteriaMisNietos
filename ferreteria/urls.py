ac6a1d139f41ac7b47c76f5254673f58a4b5b4a0
Valentino Simon <121832297+ValentinoSimon@users.noreply.github.com>
2024-06-18 19:00:01 -0300

    Change urls

--- ferreteria/urls.py
+++ ferreteria/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productos/', include('productos.urls')),
+    path('', include('productos.urls')),
 ]
--- productos/urls.py
+++ productos/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from .views import   listar_y_subir_productos_fenix , listar_y_subir_productos_palomar , listar_y_subir_productos_electrimat , lista_de_productos
 
 urlpatterns = [
-    path('lista', lista_de_productos, name='lista_de_productos'),
+    path('', lista_de_productos, name='lista_de_productos'),
     path('lista/fenix', listar_y_subir_productos_fenix, name='listar_productos_fenix'),
     path('lista/palomar', listar_y_subir_productos_palomar, name='listar_productos_palomar'),
     path('lista/electrimat', listar_y_subir_productos_electrimat, name='listar_productos_electrimat'),
